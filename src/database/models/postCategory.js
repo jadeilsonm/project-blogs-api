d2ad2d2d5e50637c16d5a3e13b301f6efca3bdb6
Jadeilson Martinho <jadeilsonm17@gmail.com>
2022-06-20 23:52:59 -0300

    fix: model de postCategory

--- src/database/models/postCategory.js
+++ src/database/models/postCategory.js
@@ -1,21 +1,35 @@
-const PostsCategorySchema = (sequelize, __DataTypes) => {
-  const PostsCategoryTable = sequelize.define('PostCategory', {});
+const PostsCategorySchema = (sequelize, DataTypes) => {
+  const PostsCategoryTable = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      foreignKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      foreignKey: true,
+    },
+  });
 
   PostsCategoryTable.associate = (models) => {
     models.BlogPost.belongsToMany(models.Category, {
-      through: postId,
-      foreignKey: '',
-      otherKey: '',
-      as: '',
+      through: PostsCategoryTable,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+      as: 'postId',
     });
 
     models.Category.belongsToMany(models.BlogPost, {
-      through: categoryId,
-      foreignKey: '',
-      otherKey: '',
-      as: '',
+      through: PostsCategoryTable,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+      as: 'categoryId',
     });
   };
+  return PostsCategoryTable
 };
 
 module.exports = PostsCategorySchema;
