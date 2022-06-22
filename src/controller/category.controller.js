b8cccb1b9274f4e8e6db74e45ea7000abfc2da89
Jadeilson Martinho <jadeilsonm17@gmail.com>
2022-06-22 18:55:16 -0300

    feat: metodo get controlle category consulta todas as categorias

--- src/controller/category.controller.js
+++ src/controller/category.controller.js
@@ -2,18 +2,27 @@ const { Router } = require('express');
 const rescue = require('express-rescue');
 const validateCategoryMiddleware = require('../middlewares/validateCategoryMiddleware');
 const validateTokenMiddleware = require('../middlewares/validateTokenMiddleware');
-const { createdCategory } = require('../services/category.services');
+const { createdCategory, getAllCategory } = require('../services/category.services');
 
 const categoryRouter = Router();
 
 categoryRouter.use(rescue(validateTokenMiddleware)); // rescue(validationCategoryMiddleware)
 
-categoryRouter.use(validateCategoryMiddleware);
-
-categoryRouter.post('/',
+categoryRouter.post(
+  '/',
+  validateCategoryMiddleware,
   rescue(async (req, res) => {
     const { dataValues } = await createdCategory(req.body);
     res.status(201).json(dataValues);
-  }));
+  }),
+);
+
+categoryRouter.get(
+  '/',
+  rescue(async (__req, res) => {
+    const payload = await getAllCategory();
+    res.status(200).json(payload);
+  }),
+);
 
 module.exports = categoryRouter;
--- src/services/category.services.js
+++ src/services/category.services.js
@@ -2,6 +2,12 @@ const { Category } = require('../database/models');
 
 const createdCategory = (payload) => Category.create(payload);
 
+const getAllCategory = () =>
+  Category.findAll({
+    attributes: ['id', 'name'],
+  });
+
 module.exports = {
   createdCategory,
+  getAllCategory,
 };
